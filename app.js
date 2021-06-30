ec61aca3771df323c3b2c2152ab3445d04293a22
Ajay Kumar <ajay.kumar@antiersolutions.com>
2021-06-30 20:02:43 +0530

    new update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var databse = require('./databas');
+require('./databas.js');
 var session = require('express-session');
 const fileUpload = require('express-fileupload');
 
--- databas.js
+++ databas.js
@@ -1,20 +1,34 @@
-// nint code
-const mongoose = require('mongoose');
-const assert = require('assert');
-const db_url = process.env.DB_URL;
+// // nint code
+// const mongoose = require('mongoose');
+// const assert = require('assert');
+// const db_url = process.env.DB_URL;
 
-// connetion code
-mongoose.connect(
-    db_url,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    },
-    function (err, link) {
-        // check errr
-        assert.equal(err, null, 'Database connection fail....');
-        // if everything is ok
-        console.log("Database connected...");
-    }
-);
+
+
+// // connetion code
+// mongoose.connect(
+//     db_url,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true
+//     },
+//     function (err, link) {
+//         // check errr
+//         assert.equal(err, null, 'Database connection fail....');
+//         // if everything is ok
+//         console.log("Database connected...");
+//     }
+// );
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://ajay:ajay@cluster0.i1mg7.mongodb.net/EmpTracker?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("database is connected");
+  // perform actions on the collection object
+  client.close();
+});
